8698e58fbb08a4478361991356513e0c327fbc6e
grisha-git <donkeevgrisa@gmail.com>
2025-11-22 18:47:04 +0300

    Getkine function

--- donkeev.grigoriy/P4/main.cpp
+++ donkeev.grigoriy/P4/main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <iomanip>
 
+char* getline(std::istream& input, size_t* size)
+{
+  bool isSkipws = input.flags() & std::ios_base::skipws;
+  if (isSkipws)
+  {
+    input >> std::noskipws;
+  }
+  size_t extendedSize = 5;
+  char* data = reinterpret_cast< char* > (malloc(sizeof(char) * extendedSize));
+  if (data == nullptr)
+  {
+    return data;
+  }
+  size_t readen = 0;
+  input >> data[0];
+  while (data[readen] != '\n')
+  {
+    if (readen == extendedSize - 1)
+    {
+      extendedSize += 5;
+      char* newData = reinterpret_cast< char* > (malloc(sizeof(char) * extendedSize));
+      if (newData == nullptr)
+      {
+        return nullptr;
+      }
+      for (size_t i = 0; i < extendedSize - 5; ++i)
+      {
+        newData[i] = data[i];
+      }
+      free(data);
+      data = newData;
+    }
+    ++readen;
+    input >> data[readen];
+  }
+  data[readen] = '\0';
+  if(isSkipws)
+  {
+    input >> std::skipws;
+  }
+  *size = readen + 1;
+  return data;
+}
 int main()
 {
-  
+  size_t size = 0;
+
+  char* array = getline(std::cin, &size);
+  if (array == nullptr)
+  {
+    std::cerr << "Memory error\n";
+    return 1;
+  }
+  std::cout << array;
+  std::cout << size;
 }
