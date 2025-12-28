68fffad7c25a2e58ea7758581028239c8fcbe093
grisha-git <donkeevgrisa@gmail.com>
2025-12-28 13:29:12 +0300

    POD-types and shape class

--- donkeev.grigoriy/P5/main.cpp
+++ donkeev.grigoriy/P5/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+namespace donkeev
+{
+  struct point_t
+  {
+    double x, y;
+  };
+  struct rectangle_t
+  {
+    double width, height;
+    point_t pos;
+  };
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() = 0;
+    virtual rectangle_t getFrameRect() = 0;
+    virtual void move(const point_t&) = 0;
+    virtual void move(const double, const double) = 0;
+    virtual void isotropicScaling(double) = 0;
+  };
 
+}
 int main()
 {
   
