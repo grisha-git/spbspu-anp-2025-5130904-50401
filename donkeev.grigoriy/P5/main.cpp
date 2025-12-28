a1bd0c44d0ebf0d7cb9c13011d62a8c0c1af0955
grisha-git <donkeevgrisa@gmail.com>
2025-12-28 15:35:02 +0300

    Rectangle class realisation

--- donkeev.grigoriy/P5/main.cpp
+++ donkeev.grigoriy/P5/main.cpp
@@ -14,13 +14,51 @@ namespace donkeev
   {
   public:
     virtual ~Shape() = default;
-    virtual double getArea() = 0;
-    virtual rectangle_t getFrameRect() = 0;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t&) = 0;
     virtual void move(const double, const double) = 0;
-    virtual void isotropicScaling(double) = 0;
+    virtual void scale(double) = 0;
   };
-
+  class Rectangle : public Shape
+  {
+  public:
+    Rectangle(const rectangle_t&);
+    ~Rectangle() override = default;
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t&) override;
+    void move(const double, const double) override;
+    void scale(double) override;
+  private:
+    rectangle_t body;
+  };
+  Rectangle::Rectangle(const rectangle_t& rectangle):
+    body(rectangle)
+  {}
+  double Rectangle::getArea() const
+  {
+    return body.height * body.width;
+  }
+  rectangle_t Rectangle::getFrameRect() const
+  {
+    return body;
+  }
+  void Rectangle::move(const point_t& target)
+  {
+    body.pos = target;
+  }
+  void Rectangle::move(const double dx, const double dy)
+  {
+    body.pos.x += dx;
+    body.pos.y += dy;
+  }
+  void Rectangle::scale(double k)
+  {
+    body.height *= k;
+    body.width *= k;
+  }
+  
 }
 int main()
 {
